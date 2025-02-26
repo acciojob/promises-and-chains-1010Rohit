d3437612dc104181a47ecd3fad989f30a50eef89
1010Rohit <parmarrohit.rp43@gmail.com>
2025-02-26 15:48:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 //your JS code here. If required.
 function handleSubmit(age,name) {
+	alert(name,age)
 	if(!age || !name ){
 		alert ( "Please enter valid details")
 		return;
 	}
 	let myPromise = new Promise((resolve,reject)=>{
-		if(age<18){
+		if(age18){
 		resolve("Oh sorry. You aren't old enough.")
 	}else{
 		reject("Welcome, . You can vote.")
 	}
-		
+	setTimeOut(()=>{
+		myPromise.then((result) => {
+    console.log(result); 
+}).catch((error) => {
+    console.error(error); // This would execute if the promise was rejected
+});
+	},4000);
 	})
 	
 }
